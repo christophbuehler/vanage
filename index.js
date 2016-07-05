94e695c5a1a4cf4f6ccc3ceb2a1498b1d90d4b36
Jan Biasi <jan.biasi@namics.com>
2016-07-05 09:58:05 +0200

    Save the service to a unique id for security reasons

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 'use strict';
 
-const uuid = require('./src/uuid');
-const Service = require('./src/common/Service');
+const uuid = require('./src/utils/uuid');
+const Service = require('./src/Service');
+const globalIdentifier = uuid();
+const semanticGlobalIdentifier = globalIdentifier.replace(/-/g, '');
 
-module.exports = new Service();
+module.exports = (function() {
+    window[semanticGlobalIdentifier] = new Service({
+        identifier: globalIdentifier,
+        debug: false
+    });
+    
+    return window[semanticGlobalIdentifier];
+})();
