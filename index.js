5990f5e1e54ae1a6f8340d0b9dda4647f4a9db79
Jan Biasi <jan.biasi@namics.com>
2016-07-05 10:46:22 +0200

    Node.js and Browser compatible now

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const Service = require('./src/Service');
 const globalIdentifier = uuid();
 const semanticGlobalIdentifier = globalIdentifier.replace(/-/g, '');
 
-module.exports = (function() {
-    window[semanticGlobalIdentifier] = new Service({
-        identifier: globalIdentifier,
-        debug: false
-    });
-    
-    return window[semanticGlobalIdentifier];
+module.exports = (function(undefined) {
+    function createInitialService() {
+        return new Service({
+            identifier: globalIdentifier,
+            debug: false
+        });
+    }
+
+    if(typeof window !== undefined) {
+        // Special security setup for browser usage which 
+        // saves the global instance to a guid in the window object
+        window[semanticGlobalIdentifier] = createInitialService();
+        return window[semanticGlobalIdentifier];
+    } else {
+        // Server uses a plain Service instance
+        return createInitialService();
+    }
 })();
